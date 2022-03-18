18df5bc74b1a28f7675ee63049eda25fe03437d9
Ni4ka1991 <astahova_nica@mail.ru>
2022-03-18 12:26:49 +0200

    class RNN starting

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from data import *
 from helper_func import *
 import torch, torch.nn as nn
 
@@ -9,3 +10,18 @@ print( text[:1000] )
 text = encodeText( text )
 print( text[:10] )
 
+x_size = len( alphabet )
+h_size = x_size
+
+class RNN( nn.Module ):
+    
+    def __init__( self, input_size, output_size, hidden_size ):
+        super( RNN, self ).__init__()
+
+
+        self.hidden_size = hidden_size
+
+
+        self.hidden = nn.Linear( input_size + hidden_size, hidden_size )
+        self.output = nn.Linear( input_size + hidden_size, output_size )
+        self.softmax = nn.LogSoftmax( dim = 1 )
