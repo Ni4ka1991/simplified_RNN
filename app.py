0005b6414bcc4f60d2c6ab0920d5e233f33957bf
Ni4ka1991 <astahova_nica@mail.ru>
2022-03-24 11:22:51 +0200

    tested. finish model rnn

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ from data import *
 from helper_func import *
 import torch, torch.nn as nn
 
-'''
+
 text = loadTextFromFile( "data/Bacteria in Daily Life.txt" )
-print( text[:1000] )
+#print( text[:1000] )
 text = encodeText( text )
-print( text[:10] )
-'''
+#print( text[:10] )
+
+
 any_numb_positive = 1.4
 
 x_size = len( alphabet )
-h_size = x_size * any_numb_positive
+#h_size = x_size * any_numb_positive
+h_size = x_size
 
 model = RNN( x_size, x_size, h_size )
 criterion = nn.MSELoss()
+optimizer = torch.optim.SGD( model.parameters(), lr = 0.005 )
