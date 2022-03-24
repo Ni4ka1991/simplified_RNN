b82a65ef56773de07bcba95928b2a7173e6cb590
Ni4ka1991 <astahova_nica@mail.ru>
2022-03-24 11:44:51 +0200

    view a input data >>> X and H tensors

--- app.py
+++ app.py
@@ -6,17 +6,27 @@ import torch, torch.nn as nn
 
 
 text = loadTextFromFile( "data/Bacteria in Daily Life.txt" )
-#print( text[:1000] )
+plain_text = loadTextFromFile( "data/Bacteria in Daily Life.txt" )
+print( text[:1000] )
 text = encodeText( text )
 #print( text[:10] )
 
 
-any_numb_positive = 1.4
+any_numb_positive = 4 # int! when the type is float appear error
 
 x_size = len( alphabet )
-#h_size = x_size * any_numb_positive
-h_size = x_size
+h_size = x_size * any_numb_positive
+#h_size = x_size
 
 model = RNN( x_size, x_size, h_size )
 criterion = nn.MSELoss()
 optimizer = torch.optim.SGD( model.parameters(), lr = 0.005 )
+
+print()
+print( f"first letter of the text >>> {plain_text[:1]}" )
+print( f"first letter of the text in vector forman >>> {text[:1]}" )
+X = torch.Tensor( text[:1] )
+print( f"first letter of the text in Torch tensor format >>>\n{X}" )
+H = model.init_hidden() # initialisation a hidden neuron with zerros
+print( f"X.shape alphabet len >>>   {X.shape} >>> 27"   )
+print( f"H.shape alphabet*4 len >>> {H.shape} >>> 27*4 = 108" )
